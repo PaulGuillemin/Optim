592e84a37059078f3dd53627e44ee4717ababc81
NewProgrammer <tristanlecourtois4@gmail.com>
2024-06-24 11:18:32 +0200

    aaaaa

--- fonction_v1.py
+++ fonction_v1.py
@@ -22,13 +22,21 @@ def testAll(p):
         service = processus[p][0]
         if useMach[m][service] == False:
             if testRessources(p,m)== True:
+                useMach[m][service] = True 
                 return m
     return machineneutre
 
 def testNewLoc(p):
+    useLoc = [[False for i in range(nbServices)] for j in range(nbMachines)]
     service = processus[p][0]
     for m in range(nbMachines):
-        
+        loc = machines[m]
+        if not useLoc[loc][service]:
+            if testRessources(p, m):
+                useLoc[loc][service] = True
+                return m
+    return machineneutre
+
 
 
 
